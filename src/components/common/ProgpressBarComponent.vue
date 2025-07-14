e8f125e317a0b4f0f2b3688a3413d9ec33464605
Aljune <54563642+Aljune@users.noreply.github.com>
2025-07-14 19:17:33 +0800

    Update ProgpressBarComponent.vue
    Update ProgpressBarComponent

--- src/components/common/ProgpressBarComponent.vue
+++ src/components/common/ProgpressBarComponent.vue
@@ -6,7 +6,7 @@
                     <div class="col-12">
                         <div class="text-end">
                             <h5 class=" text-start text-white">
-                                BOOTSTRAP 5
+                                BOOTSTRAP 4 OR 5 / MUI / TAILWIND
                             </h5>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
@@ -16,7 +16,7 @@
                     <div class="col-12">
                         <div class="text-end">
                             <h5 class=" text-start text-white">
-                                CSS375
+                                CSS375 / SASS
                             </h5>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -36,7 +36,7 @@
                     <div class="col-12">
                         <div class="text-end">
                             <h5 class=" text-start text-white">
-                                PHP LARAVEL 7/10
+                                PHP LARAVEL 7/10 And Sactum
                             </h5>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -67,6 +67,26 @@
                         <div class="text-end">
                             <h5 class=" text-start text-white">
                                 ADOBE PHOTOSHOP CS6
+                            </h5>
+                            <div class="progress">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="text-end">
+                            <h5 class=" text-start text-white">
+                                FIGMA DESIGN
+                            </h5>
+                            <div class="progress">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="80"></div>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="col-12">
+                        <div class="text-end">
+                            <h5 class=" text-start text-white">
+                                POSTMAN
                             </h5>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -141,7 +161,27 @@
                     <div class="col-12">
                         <div class="text-end">
                             <h5 class=" text-start text-white">
-                                ADOBE ILLUSRATOR
+                                Next JS
+                            </h5>
+                            <div class="progress">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="text-end">
+                            <h5 class=" text-start text-white">
+                                Next JS
+                            </h5>
+                            <div class="progress">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="text-end">
+                            <h5 class=" text-start text-white">
+                                SQL / Firebase / Prisma / Hasura
                             </h5>
                             <div class="progress">
                                 <div class="progress-bar bg-success" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
